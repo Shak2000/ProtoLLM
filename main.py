7418c559d542b5d0d44e28f3a5a92f92605376de
Shak Ragoler <shak.ragoler@gmail.com>
2025-07-01 14:15:22 -0700

    Outline of a Proto-LLM

---
+++ main.py
@@ -0,0 +1,86 @@
+import os
+import random
+
+
+class ProtoLLM:
+    def __init__(self):
+        self.word_map = dict()
+
+    def train(self, text):
+        word_list = text.split()
+
+        for i in range(len(word_list) - 1):
+            if word_list[i] not in self.word_map.keys():
+                self.word_map[word_list[i]] = dict()
+                self.word_map[word_list[i]]["Grand Total"] = 0
+            if word_list[i + 1] not in self.word_map[word_list[i]]:
+                self.word_map[word_list[i]][word_list[i + 1]] = 0
+
+            self.word_map[word_list[i]][word_list[i + 1]] += 1
+            self.word_map[word_list[i]]["Grand Total"] += 1
+
+        self.word_map[word_list[-1]] = dict()
+
+    def next_word(self, curr_word):
+        if curr_word not in self.word_map.keys():
+            return random.choice(list(self.word_map.keys()))
+
+        index = random.randint(0, self.word_map[curr_word]["Grand Total"] - 1)
+        track = 0
+        for word in self.word_map[curr_word].keys():
+            if word != "Grand Total":
+                track += self.word_map[curr_word][word]
+                if track > index:
+                    return word
+        return ""
+
+    def end_of_sentence(self, word):
+        return len(word) > 0 and (word[-1] == "." or word[-1] == "!" or word[-1] == "?")
+
+    def gen_word_list(self, word_list, energy=0.8):
+        start_len = len(word_list)
+        while True:
+            if self.end_of_sentence(word_list[-1]) and len(word_list) > start_len and random.random() > energy:
+                break
+            word_list.append(self.next_word(word_list[-1]))
+        return word_list
+
+    def gen_message(self, message):
+        word_list = self.gen_word_list(message.split())
+        message = ""
+        for i in range(len(word_list) - 1):
+            message += word_list[i] + " "
+        return message + word_list[-1]
+
+
+def main():
+    print("Welcome to the Proto-LLM app!")
+    llm = ProtoLLM()
+
+    while True:
+        print()
+        choice = input("Would you like to (1) enter a training file, (2) query a prompt, or (3) quit the program? ")
+
+        if choice == "1":
+            file_name = input("Please enter a training file: ")
+            if os.path.exists(file_name):
+                file = open(file_name, "r+")
+                llm.train(file.read())
+                print(f"Thank you for letting the Proto-LLM train on {file_name}!")
+            else:
+                print(f"{file_name} does not exist. Please try again.")
+
+        elif choice == "2":
+            prompt = input("Please enter a prompt: ")
+            print("The Proto-LLM is thinking...")
+            print(llm.gen_message(prompt))
+
+        elif choice == "3":
+            break
+
+        else:
+            print(f"{choice} is not a valid input. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
